668e94b09577a2c3636f352d5680ff4a63389081
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-03-15 16:07:53 +0200

    Update Docker configurations and environment settings
    Add new Docker Compose files for development and testing environments.
    Update PORT value in .env file for testing environment.
    Adjust PORT value in src/index.js for better testing setup.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 3001
 const NODE_ENV = process.env.NODE_ENV
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World!!!!!!!!!')
 });
 app.listen(PORT, () => {
     console.log(`Server up and running on port ${PORT} in ${NODE_ENV} mode`);
