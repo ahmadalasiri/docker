19a4f9f37354331283674bc880db142e52fbd6de
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2024-03-15 09:59:04 +0200

    Add docker-compose.yml and update start scripts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3000;
 // create express app and listen on port 4000 
 
 const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World')
 });
-
-app.listen(80, () => {
-    console.log(`Server is running on port 80`);
+app.listen(PORT, () => {
+    console.log(`Server up and running http://localhost:${PORT}`);
 })
